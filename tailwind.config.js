99fb06636e5a718b22e40b2595b11d7f4721bc55
tian <istiancz@gmail.com>
2024-10-18 16:57:46 +0800

    fix: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -100,11 +100,6 @@ export default {
 					foreground: 'hsl(var(--card-foreground))',
 				},
 			},
-			borderRadius: {
-				lg: `var(--radius)`,
-				md: `calc(var(--radius) - 2px)`,
-				sm: 'calc(var(--radius) - 4px)',
-			},
 			fontFamily: {
 				sans: ['var(--font-sans)'],
 			},
